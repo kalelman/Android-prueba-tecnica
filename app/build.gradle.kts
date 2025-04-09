a9cab700b66166eea4b66f0dc4be753b8432ae52
Erick Rojas Perez <erick_rojas_perez@hotmail.com>
2025-04-09 14:33:51 -0600

    Adding libraries to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // MVVM & Lifecycle
+    implementation (libs.lifecycle.livedata.ktx)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Retrofit & Gson
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
+    // Coroutines
+    implementation (libs.kotlinx.coroutines.android)
+
+    // Hilt
+    implementation (libs.hilt.android)
 }
