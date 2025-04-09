38628d1f95cbcc5300e300eae553582a81a6f01b
Erick Rojas Perez <erick_rojas_perez@hotmail.com>
2025-04-09 17:36:39 -0600

    LoginActivity added

--- app/src/main/java/com/example/androidtest/ui/theme/login/LoginActivity.kt
+++ app/src/main/java/com/example/androidtest/ui/theme/login/LoginActivity.kt
@@ -1,4 +1,24 @@
 package com.example.androidtest.ui.theme.login
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtest.R
+import com.example.androidtest.ui.theme.employeelist.EmployeeListActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            val id = findViewById<EditText>(R.id.inputUserId).text.toString()
+            if (id.isNotEmpty()) {
+                startActivity(Intent(this, EmployeeListActivity::class.java))
+                finish()
+            }
+        }
+    }
 }
